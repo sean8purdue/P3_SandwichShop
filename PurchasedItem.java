fdaea926a3343f28fc364214800b9d304626eff4
Sean <sean8purdue@gmail.com>
2016-03-06 11:43:22 -0500

    1.1 Purchased Item Interface Created

--- PurchasedItem.java
+++ PurchasedItem.java
@@ -1,6 +1,12 @@
 public interface PurchasedItem {
 
     // TO-DO
+    public boolean isDelivery();
+    public String getCustomerName();
+    public int getDeliveryTime();
+    public void setDeliveryTime(int time);
+    public double getMaterialCost();
+    public double getSalePrice();
 
 }
 
